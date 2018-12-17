312f45041562941124d9489201e1db381b352172
Block-Build <Block-Build@t-online.de>
2018-12-17 05:00:16 +0100

    fix formatting

--- src/main/java/de/blockbuild/musikbot/Bot.java
+++ src/main/java/de/blockbuild/musikbot/Bot.java
@@ -11,45 +11,42 @@ import net.dv8tion.jda.core.entities.Game;
 import net.dv8tion.jda.core.entities.Game.GameType;
 
 public class Bot {
-	
+
 	private JDA jda;
-	
+
 	public Bot() {
 		start();
 		initListener();
 	}
-	
+
 	public boolean start() {
 		try {
 			jda = new JDABuilder(AccountType.BOT)
 					.setToken("NTIzOTI3MzY3NDY3NjYzNDAx.Dvh6cg.r6rrETJfOYRBJp2Xc3l-zPn_BuY")
-					.setGame(Game.of(GameType.DEFAULT, "Loding"))
-					.setAudioEnabled(true)
-					.setStatus(OnlineStatus.DO_NOT_DISTURB)
-					.build();
+					.setGame(Game.of(GameType.DEFAULT, "starting...")).setAudioEnabled(true)
+					.setStatus(OnlineStatus.DO_NOT_DISTURB).build();
 			jda.awaitReady();
-			
-			System.out.println("Finished Building JDA");
-			return true;
+
+			System.out.println("Bot started successfully");
 		} catch (LoginException e) {
-			System.out.println(e);
+			// System.out.println(e);
 			System.out.println("Invaild bot Token");
 			return false;
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			return false;
 		}
-        catch (InterruptedException e)
-        {
-            e.printStackTrace();
-            return false;
-            }
+		jda.getPresence().setPresence(OnlineStatus.ONLINE, Game.of(GameType.DEFAULT, "Ready for playing music. !Play"));
 
-	}
-	
-	public boolean stop() {
-		jda.shutdown();
-    	System.out.println("Finished JDA shutdown");
 		return true;
 	}
-	
+
+	public boolean stop() {
+		jda.shutdown();
+		System.out.println("Bot has stopped");
+		return true;
+	}
+
 	public void initListener() {
 		jda.addEventListener(new MessageListener());
 	}
--- src/main/java/de/blockbuild/musikbot/Main.java
+++ src/main/java/de/blockbuild/musikbot/Main.java
@@ -3,40 +3,40 @@ package de.blockbuild.musikbot;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin{
-	
+public class Main extends JavaPlugin {
+
 	Bot bot;
-	
+
 	@Override
 	public void onEnable() {
 		try {
 			start();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
-	
-    @Override
-    public void onDisable() {
-        if (bot != null) {
-            if(bot.stop()) {
-            }
-        }
-    }
-	
-    private void start() {
-        //initInstances();
-        Bukkit.getScheduler().runTaskLater(this, () -> {
-            if (true /*mcbConfigsManager.setupConfigurations()*/ ) {
-                //bot = new Bot(this, eventWaiter);
-            	System.out.println("Musik Bot wird gestartet");
-            	bot = new Bot();
-            } else {
-                this.getServer().shutdown();
-            }
-        }, 1L);
-        //Bukkit.getScheduler().runTaskLater(this, () ->
-        //        Bukkit.getPluginManager().callEvent(new StartEvent(this)), 20L);
 
-    }
+	@Override
+	public void onDisable() {
+		if (bot != null) {
+			if (bot.stop()) {
+			}
+		}
+	}
+
+	private void start() {
+		// initInstances();
+		Bukkit.getScheduler().runTaskLater(this, () -> {
+			if (true /* mcbConfigsManager.setupConfigurations() */ ) {
+				// bot = new Bot(this, eventWaiter);
+				System.out.println("Musik Bot wird gestartet");
+				bot = new Bot();
+			} else {
+				this.getServer().shutdown();
+			}
+		}, 1L);
+		// Bukkit.getScheduler().runTaskLater(this, () ->
+		// Bukkit.getPluginManager().callEvent(new StartEvent(this)), 20L);
+
+	}
 }
