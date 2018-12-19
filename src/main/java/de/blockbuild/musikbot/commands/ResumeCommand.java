b28fbfac013f2611e2a1ccde90d08dffb0add982
Block-Build <Block-Build@t-online.de>
2018-12-19 22:19:52 +0100

    add Pause and Resume Commands

---
+++ src/main/java/de/blockbuild/musikbot/commands/ResumeCommand.java
@@ -0,0 +1,33 @@
+package de.blockbuild.musikbot.commands;
+
+import com.jagrosh.jdautilities.commandclient.CommandEvent;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+
+import de.blockbuild.musikbot.Main;
+import de.blockbuild.musikbot.core.MBCommand;
+
+import net.dv8tion.jda.core.OnlineStatus;
+import net.dv8tion.jda.core.entities.Game;
+import net.dv8tion.jda.core.entities.Game.GameType;
+
+public class ResumeCommand extends MBCommand {
+
+	public ResumeCommand(Main main) {
+		super(main);
+		this.name = "resume";
+		this.help = "Resume playback.";
+	}
+
+	@Override
+	protected void doCommand(CommandEvent event) {
+		if (!(event.getGuild().getAudioManager().isConnected())) {
+			//not yet Tested!!!
+			event.getGuild().getAudioManager().openAudioConnection(event.getSelfMember().getVoiceState().getChannel());
+		}
+
+		AudioPlayer player = main.getBot().getScheduler().getPlayer();
+		player.setPaused(false);
+		event.getJDA().getPresence().setStatus(OnlineStatus.ONLINE);
+		event.getJDA().getPresence().setGame(Game.of(GameType.DEFAULT, player.getPlayingTrack().getInfo().title));
+	}
+}
