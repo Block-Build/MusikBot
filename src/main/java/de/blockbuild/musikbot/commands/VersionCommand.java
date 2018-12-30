064379803efd8edaada490778f1cf57f4882715b
Block-Build <Block-Build@t-online.de>
2018-12-30 20:08:34 +0100

    add VersionCommand

---
+++ src/main/java/de/blockbuild/musikbot/commands/VersionCommand.java
@@ -0,0 +1,24 @@
+package de.blockbuild.musikbot.commands;
+
+import com.jagrosh.jdautilities.commandclient.CommandEvent;
+
+import de.blockbuild.musikbot.Main;
+import de.blockbuild.musikbot.core.MBCommand;
+
+public class VersionCommand extends MBCommand {
+
+	public VersionCommand(Main main) {
+		super(main);
+		this.name = "version";
+		this.help = "MusikBot version.";
+		this.joinOnCommand = false;
+		this.category = SETUP;
+	}
+
+	@Override
+	protected void doCommand(CommandEvent event) {
+		StringBuilder builder = new StringBuilder().append(event.getClient().getSuccess());
+		builder.append(main.getDescription().getFullName());
+		event.reply(builder.toString());
+	}
+}
