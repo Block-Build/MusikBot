2c6366be86dc3764b3393f857144a55d29a09283
Block-Build <Block-Build@t-online.de>
2019-01-13 16:37:06 +0100

    remove some unused permissions

--- src/main/java/de/blockbuild/musikbot/Bot.java
+++ src/main/java/de/blockbuild/musikbot/Bot.java
@@ -60,13 +60,10 @@ import net.dv8tion.jda.core.entities.VoiceChannel;
 public class Bot {
 	public final static Permission[] RECOMMENDED_PERMS = new Permission[] { Permission.MESSAGE_READ,
 			Permission.MESSAGE_WRITE, Permission.MESSAGE_HISTORY, Permission.MESSAGE_ADD_REACTION,
-			Permission.MESSAGE_EMBED_LINKS, Permission.MESSAGE_ATTACH_FILES, Permission.MESSAGE_MANAGE,
-			Permission.MESSAGE_EXT_EMOJI, Permission.MANAGE_CHANNEL, Permission.VOICE_CONNECT, Permission.VOICE_SPEAK,
-			Permission.NICKNAME_CHANGE, Permission.MESSAGE_TTS };
+			Permission.MESSAGE_EMBED_LINKS, Permission.VOICE_CONNECT, Permission.VOICE_SPEAK, Permission.MESSAGE_TTS };
 	public final static Permission[] REQUIRED_PERMS = new Permission[] { Permission.MESSAGE_READ,
 			Permission.MESSAGE_WRITE, Permission.MESSAGE_ADD_REACTION, Permission.MESSAGE_EMBED_LINKS,
-			Permission.MESSAGE_MANAGE, Permission.MESSAGE_EXT_EMOJI, Permission.VOICE_CONNECT, Permission.VOICE_SPEAK,
-			Permission.MESSAGE_TTS };
+			Permission.VOICE_CONNECT, Permission.VOICE_SPEAK, Permission.MESSAGE_TTS };
 	private final Main main;
 	private final AudioPlayerManager playerManager;
 	private final Map<Long, GuildMusicManager> musicManagers;
