a06881aced71da3034751f489b1f03149c6bef5f
Block-Build <Block-Build@t-online.de>
2019-01-29 23:01:10 +0100

    expand Version Command

--- src/main/java/de/blockbuild/musikbot/commands/VersionCommand.java
+++ src/main/java/de/blockbuild/musikbot/commands/VersionCommand.java
@@ -18,7 +18,13 @@ public class VersionCommand extends MBCommand {
 	@Override
 	protected void doCommand(CommandEvent event) {
 		StringBuilder builder = new StringBuilder().append(event.getClient().getSuccess());
-		builder.append(" ").append(bot.getMain().getDescription().getFullName());
+
+		builder.append(" **MusikBot** ").append("by Block-Build\n");
+		builder.append("SpigotMC: `https://www.spigotmc.org/resources/the-discord-musikbot-on-minecraft.64277/`\n");
+		builder.append("GuitHub: `https://github.com/Block-Build/MusikBot`\n");
+		builder.append("Version: `").append(bot.getMain().getDescription().getVersion()).append("`\n");
+		builder.append("Do you have any problem or suggestion? Write me on https://github.com/Block-Build/MusikBot");
+
 		event.reply(builder.toString());
 	}
 }
