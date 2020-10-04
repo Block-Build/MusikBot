a796768d9758d711a9c97f77b5c44cc34fda7126
Block-Build <Block-Build@t-online.de>
2020-10-04 14:24:15 +0200

    update main

--- src/main/java/de/blockbuild/musikbot/Main.java
+++ src/main/java/de/blockbuild/musikbot/Main.java
@@ -23,39 +23,8 @@ public class Main extends JavaPlugin {
 	public void onEnable() {
 		try {
 			setFilePath();
-
 			start();
-
-			Metrics metrics = new Metrics(this);
-
-			metrics.addCustomChart(new Metrics.SingleLineChart("guilds_lin", () -> bot.getJda().getGuilds().size()));
-
-			metrics.addCustomChart(
-					new Metrics.SimplePie("guilds_pie", () -> String.valueOf(bot.getJda().getGuilds().size())));
-
-			metrics.addCustomChart(new Metrics.SingleLineChart("streams_lin", () -> {
-				int counter = 0;
-				for (Guild guild : bot.getJda().getGuilds())
-					if (bot.getGuildAudioPlayer(guild).getAudioPlayer().getPlayingTrack() != null) {
-						counter++;
-					}
-				return counter;
-			}));
-
-			metrics.addCustomChart(new Metrics.SingleLineChart("users_lin", () -> bot.getJda().getUsers().size()));
-
-			metrics.addCustomChart(new Metrics.SingleLineChart("users_online_lin", () -> {
-				Set<User> users = new HashSet<User>();
-				for (Guild guild : bot.getJda().getGuilds()) {
-					guild.getMembers().forEach((member) -> {
-						if (member.getOnlineStatus() != OnlineStatus.OFFLINE
-								&& member.getOnlineStatus() != OnlineStatus.UNKNOWN) {
-							users.add(member.getUser());
-						}
-					});
-				}
-				return users.size();
-			}));
+			Metrics();
 		} catch (Exception e) {
 			System.out.println(e);
 		}
@@ -78,6 +47,39 @@ public class Main extends JavaPlugin {
 		Bukkit.getScheduler().runTaskLater(this, () -> {
 			bot = new Bot(this);
 		}, 1L);
+	}
+
+	private void Metrics() {
+		Metrics metrics = new Metrics(this);
+
+		metrics.addCustomChart(new Metrics.SingleLineChart("guilds_lin", () -> bot.getJda().getGuilds().size()));
+
+		metrics.addCustomChart(
+				new Metrics.SimplePie("guilds_pie", () -> String.valueOf(bot.getJda().getGuilds().size())));
+
+		metrics.addCustomChart(new Metrics.SingleLineChart("streams_lin", () -> {
+			int counter = 0;
+			for (Guild guild : bot.getJda().getGuilds())
+				if (bot.getGuildAudioPlayer(guild).getAudioPlayer().getPlayingTrack() != null) {
+					counter++;
+				}
+			return counter;
+		}));
+
+		metrics.addCustomChart(new Metrics.SingleLineChart("users_lin", () -> bot.getJda().getUsers().size()));
+
+		metrics.addCustomChart(new Metrics.SingleLineChart("users_online_lin", () -> {
+			Set<User> users = new HashSet<User>();
+			for (Guild guild : bot.getJda().getGuilds()) {
+				guild.getMembers().forEach((member) -> {
+					if (member.getOnlineStatus() != OnlineStatus.OFFLINE
+							&& member.getOnlineStatus() != OnlineStatus.UNKNOWN) {
+						users.add(member.getUser());
+					}
+				});
+			}
+			return users.size();
+		}));
 	}
 
 	private final void setFilePath() {
